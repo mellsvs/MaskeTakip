e33abc758b378f05a0b19259dd9246d9bd29f348
meltemSavas <you@example.com>
2024-02-05 22:47:41 +0300

    .
    .

---
+++ MaskeTakipKamp/MaskeTakip/Business/Abstract/IApplicantService.cs
@@ -0,0 +1,12 @@
+﻿namespace Business.Abstract
+{
+    public interface IApplicantService
+    {
+        void ApplyForMask(Person person);
+
+        List<Person> GetList();
+
+        bool CheckPerson(Person person);
+    }
+}
+
---
+++ MaskeTakipKamp/MaskeTakip/Business/Concrete/PersonManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Business.Abstract;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        //encapsulation
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(
+                new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+
+        }
+    }
+}
+
